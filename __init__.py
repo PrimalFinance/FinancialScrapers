d6adedfbf64f2a14cfe67f22e554660471132c81
PrimalFinance <primalfinance101@gmail.com>
2024-02-01 23:33:05 -0800

    Initial commit.

--- __init__.py
+++ __init__.py
@@ -1,8 +1,1 @@
 from DataManager.data_manager import DataManager
-
-
-if __name__ == "__main__":
-    d = DataManager("D:\\FinancialData\\FinancialData")
-
-    data = d.get_fed_funds()
-    print(f"Data: {data}")
