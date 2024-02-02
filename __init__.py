bc1e7a1e9885dc697efeae623661092b15bf647b
PrimalFinance <primalfinance101@gmail.com>
2024-02-02 11:20:42 -0800

    Updated.

--- __init__.py
+++ __init__.py
@@ -2,8 +2,4 @@ from DataManager.data_manager import DataManager
 
 
 if __name__ == "__main__":
-
     d = DataManager("D:\\FinancialData\\FinancialData")
-    tickers = d.get_ticker_list()
-    for t in tickers:
-        d.get_filing_dates(t)
